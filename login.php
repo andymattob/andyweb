987c3fcead39ab84302755ee590c0bc8c564f041
Andreas Johansson <86415353+andymattob@users.noreply.github.com>
2023-11-25 01:24:23 +0100

    test

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8"/>
-    <title>Logga in</title>
+    <title>Gamesphere - Logga in</title>
     <link rel="stylesheet" href="css/style.css"/>
 </head>
 <body>
